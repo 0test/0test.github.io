6d1c55d6682b3c7cd25c9218c405ccb3605f70a2
0test <0test@mail.ru>
2019-09-27 18:48:03 +0300

    1

---
+++ js/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+	var access_cookie_name = 'save_params';
+
+	$('.my_cow').click(function(){
+		values = Cookies.getJSON(access_cookie_name);
+		Cookies.remove(access_cookie_name);
+		Cookies.set(access_cookie_name,{
+			bg: values['bg'],
+			font: $(this).attr('rel'),
+			img:values['img'],
+			fonttype: values['fonttype']
+		});
+		return false;
+	});
+
+	
+
+	if(Cookies.get(access_cookie_name)){
+		values = Cookies.getJSON(access_cookie_name);
+		console.log(values);
+	}
+	else{
+		Cookies.set(access_cookie_name,{
+			bg: 0,
+			items: {1,2,3,4}
+		});
+	}
+	
+});
