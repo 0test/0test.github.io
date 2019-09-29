d00f96348acd50fdbd12548d49a8db0620847d09
0test <0test@mail.ru>
2019-09-29 19:16:29 +0300

    a

--- js/custom.js
+++ js/custom.js
@@ -1,28 +1,47 @@
 $(document).ready(function() {
 	var access_cookie_name = 'save_params';
-
-	$('.my_cow').click(function(){
-		that = this;
-		values = Cookies.getJSON(access_cookie_name);
-		Cookies.remove(access_cookie_name);
-		Cookies.set(access_cookie_name,{
-			bg: 1,
-			items: [1,2,3,4,5]
-		});
-		return false;
-	});
-
+	var eachItems = [];
 	
 
 	if(Cookies.get(access_cookie_name)){
 		values = Cookies.getJSON(access_cookie_name);
-		console.log(values);
+		eachItems = values;
 	}
 	else{
 		Cookies.set(access_cookie_name,{
-			bg: 0,
 			items: []
 		});
 	}
+	console.log(eachItems);
 	
+	$('.my_cow').click(function(){
+		that = this;
+		var eid = $(that).parent().attr('data-id');
+		if( eachItems.indexOf(eid,0) == -1){
+			eachItems.push( eid );
+		}
+		else{
+			eachItems.splice( eachItems.indexOf(eid,0),1 );
+		}
+		Cookies.remove(access_cookie_name);
+		Cookies.set(access_cookie_name,{
+			items: eachItems
+		});
+		reloadClasses(eachItems);
+		return false;
+	});
+	function reloadClasses(eachItems){
+		$('li[data-id]').removeClass('green');
+		$('li[data-id]').each(function(i,e){
+			console.log(eachItems.indexOf(  $(e).attr('data-id') , 0) );
+			if( eachItems.indexOf( $(e).attr('data-id'), 0)  != -1 ){
+
+				$(e).addClass('green');
+			}
+		});
+	}
+
+
+	
+
 });
